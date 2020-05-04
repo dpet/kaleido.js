8d1e08be7e518fd721fe0d0e04fc3f51617f0c8c
Daniel Petersen <dpetersen1@live.com>
2020-05-04 00:05:12 -0700

    framerate to 30

--- README.md
+++ README.md
@@ -22,6 +22,34 @@ This is a simple script that adds a kaleidoscope effect to a div. Just a pass th
 </script>
 ```
 
+## config
+
+if the config object isn't passed or any of the properties aren't passes then defaults will be used;
+
+- zoom
+	- default: 1.6 
+	- stretches the image so it covers the canvas when rotated. A zoom of 1 leaves the image at the size of the canvas so the background can be seen when it turns. How much the image should be zoomed depends on the shape of the canvas, if it is rectangle then the image may need to be zoomed more. If you zoom too much then the image will be pixelated.
+
+- start_angle
+	- default: 0
+	- starts with image already rotated to a certain degree. Use 180 to have it upside down etc.
+
+- speed
+	- default: 1
+	- how fast it rotates.
+
+- pulse
+	- default: 0
+	- causes the image to zoom in and out smoothly. 1 is very subtle and 10 makes it very obvious.
+
+- horizontal_num
+	- default: 4
+	- how many images are fit into a row.
+
+- vertical_num
+	- default: 2
+	- how many images are fit into a column.
+
 ## links
 
 See https://www.danielpetersen.ca/kaleido for a demo project that lets you experiment with different arrangements. 
@@ -34,7 +62,7 @@ Another one https://www.danielpetersen.ca/liquid
 
 ## Notes
 
-The image used should be square, the extra length of the longer side will be unused if not.
+The image used should be square, the extra length of the longer side will be unused if not. Width is used in some calculations so you may need to zoom more if the image is a rectangle.
 
 Smaller images are easier on the hosts computer, I scaled the demo images to 600px x 600px.
 
@@ -42,7 +70,4 @@ The animation continues to run when the div isn't on the screen, this should be 
 
 There are no mobile fallbacks so you need to add your own, ie. put an image in the div instead of calling kaleido_start().
 
-The zoom property in the config object may be necessary to stretch the image over the full area of the canvas. If there is white space then increase the zoom. If you zoom too much ten the image will be pixelated. 
-
-pulse zooms the image in and out a bit.
 
--- kaleido.js
+++ kaleido.js
@@ -10,7 +10,7 @@ function kaleido_start(parent_div, image_path, config = {}){
 
 	let zoom = config.hasOwnProperty('zoom') ? parseFloat(config.zoom) : 1.6;
 	let applied_zoom = zoom;
-	let zoom_counter = 0;
+	let pulse_counter = Math.PI; // cos used for pulse and PI lets it start at the bottom
 
 	let rotation = config.hasOwnProperty('start_angle') ? parseInt(config.start_angle) : 0;
 	let speed = config.hasOwnProperty('speed') ? parseFloat(config.speed) : 1;
@@ -20,7 +20,7 @@ function kaleido_start(parent_div, image_path, config = {}){
 	let vertical_num = config.hasOwnProperty('vertical_num') ? parseInt(config.vertical_num) : 2;
 	let total_images = horizontal_num * vertical_num
 
-	let imgScaledWidth, holder, canvas_width, canvas_height;
+	let imgScaledWidth, holder, canvas_width, canvas_height, last_timestamp;
 	let canvasses = []
 	let img = new Image();
 
@@ -72,25 +72,29 @@ function kaleido_start(parent_div, image_path, config = {}){
 	}
 
 	function start(){
+		last_timestamp = 0;
 		window.requestAnimationFrame(draw_image);
 	};
 
 	function draw_image(timestamp){
-		rotation += speed/3000;
-		set_zoom();
 
-		if(typeof start_time === 'undefined') start_time = timestamp;
-  		var progress = timestamp - start_time;
+		if(timestamp - last_timestamp > 20){
+			rotation += speed/30;
 
-  		start_time = timestamp;
+			set_zoom();
 
-		// looks at the canvasses position to decide how it gets flipped
-		for (let a=0; a<total_images; a++){
-			let left = a % horizontal_num % 2 == 0
-			let top = Math.floor(a/horizontal_num) % 2 == 0
+			// looks at the canvasses position to decide how it gets flipped
+			for (let a=0; a<total_images; a++){
+				let left = a % horizontal_num % 2 == 0
+				let top = Math.floor(a/horizontal_num) % 2 == 0
 
-			draw_context(canvasses[a].context, left, top)
+				draw_context(canvasses[a].context, left, top)
+			}
+
+			last_timestamp = timestamp;
 		}
+
+		
 
 		if (!holder.getAttribute('data-stop'))
 			window.requestAnimationFrame(draw_image);
@@ -123,15 +127,17 @@ function kaleido_start(parent_div, image_path, config = {}){
 		context.scale(hor, vert);	
 		
 		context.translate(imgScaledWidth/2, canvas_height/2);
-		context.rotate(rotation);			
+		context.rotate(rotation * Math.PI / 180);			
 		context.translate(-imgScaledWidth/2, -canvas_height/2);
 		
 		context.drawImage(img, 0, 0, imgScaledWidth, canvas_height);
 	}
 
 	function set_zoom(){
-		applied_zoom = zoom + (.5 + Math.sin(zoom_counter)) / 14
-		zoom_counter += pulse / 600;
+		if (pulse != 0){
+			applied_zoom = zoom * (1 + (1 + Math.cos(pulse_counter)) / 16)
+			pulse_counter += pulse / 300;
+		}
 	}
 
 	// adds a new image to canvasses
